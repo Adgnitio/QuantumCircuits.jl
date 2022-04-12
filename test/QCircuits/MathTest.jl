0614035b88d0d75e4170de4a12f54829a503c8e2
Rafał Pracht <rpracht@wp.pl>
2022-04-12 10:20:15 +0200

    Improve unitary_error function for better performance

--- test/QCircuits/MathTest.jl
+++ test/QCircuits/MathTest.jl
@@ -1,3 +1,4 @@
+
 # This code is part of QuantumCircuits.
 #
 # (C) Copyright Rafał Pracht 2022.
@@ -10,12 +11,21 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-include("MathTest.jl")
-include("RegistersTest.jl")
-include("CircuitTest.jl")
-include("InstructionsTest.jl")
-include("CircuitSimplifyTest.jl")
-include("CircuitUnitaryTest.jl")
-include("GatesTest.jl")
-include("OtherGatesTest.jl")
-include("CircuitMeasureTest.jl")
+using Test
+
+using QuantumCircuits.QCircuits.Math
+
+
+function test_unitary_error(exp_unit, mat_unit)
+    dif = exp_unit - mat_unit
+    return matrix_norm(dif)
+end
+
+A = [1+im 2+im; 3+im 4+im]
+B = [1-im 2-im; 3-im 4-im]
+
+@test abs(test_unitary_error(A, B) - unitary_error(A, B)) < 1e-6
+
+A = rand(64, 64)
+B = rand(64, 64)
+@test abs(test_unitary_error(A, B) - unitary_error(A, B)) < 1e-6
